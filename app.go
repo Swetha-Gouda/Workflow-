2b0a4ebdd3461a699bb316d2ba96efce5dddbcea
Swetha <swethagouda4@gmail.com>
2026-04-10 16:13:32 +0530

    new features

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Fiber API")
+		return c.SendString("about page context")
 	})
 
 	app.Listen(":3000")
